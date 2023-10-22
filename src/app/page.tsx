e3bbe1b30b3a0a9534e756bd46bbaa2f183c249a
rodres101 <rodrigosergio93@gmail.com>
2023-10-22 18:49:39 -0400

    add images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,22 +41,50 @@ export default function Home() {
     setIsDark(!isDark)
   }
   const label = "FOR \n HAIR & BEARD"
+  const images = []
+  for(let i=1; i<=14; i++){ 
+    if(i<10){
+      images.push(`00${i}.jpg`) 
+    }else{
+      images.push(`0${i}.jpg`) 
+    }    
+  }
+  console.log(images);
   return (
-    <body className={`flex flex-col min-h-screen ${isDark? "bg-black": "bg-white"}`}>
+    <body 
+      className={`flex flex-col min-h-screen ${isDark? "bg-black": "bg-white"}`}
+      >
       <div 
         className={`${chunkeFont.variable} flex flex-col place-content-center h-full items-center content-center`}
-        onClick={clicking}
-        >
-        <LogoComponent fill={`${isDark? "white": "black"}`}></LogoComponent>
-        {/* <Image
-          src="/forhairandbeard.svg"
-          alt="ForHairAndBear"
-          className={`dark:invert pb-10 ${isDark? "bg-black": "bg-white"}`}
-          width={300}
-          height={24}
-          priority
-        /> */}
-        <div className={`font-chunke md:text-5xl sm:text-4xl whitespace-pre-line text-center ${isDark? "text-white": ""}`}>{label}</div>          
+        onClick={clicking}>
+        <div>
+          <LogoComponent fill={`${isDark? "white": "black"}`}></LogoComponent>
+          <div 
+            className={`font-chunke md:text-5xl sm:text-4xl whitespace-pre-line text-center ${isDark? "text-white": ""}`}>
+            {label}
+          </div>
+        </div>
+      </div>
+      <div className='mt-auto flex content-center place-content-center pt-15 pb-5'>
+        <ol>
+          {
+            images.map((el,key) => (
+              <li key={`i-${key}`}>
+                <div
+                  className='p-3'>
+                  {<Image 
+                    src={`/img/${el}`}
+                    alt={`ilustration-${el}`}
+                    // className=''
+                    width={200}
+                    height={100}
+                    priority
+                  />}
+                </div>
+              </li>
+            ))
+          }
+        </ol>
       </div>
       <footer className="mt-auto flex content-center place-content-center pt-15 pb-5">
         <div className={`md:text-2xl sm:text-sm ${isDark? "text-white": ""}`}>Cochabamba - 2023</div>
